c98d27f106f7fac5a536b43fb9c1bb0b98c067dc
kubaferg <kfergus1@macalester.edu>
2025-09-15 10:16:15 -0500

    commit

---
+++ src/ica/04-Functions/difference.py
@@ -0,0 +1,7 @@
+def smallest_diff(x, y, z):
+    diff1 = abs(x-y)
+    diff2 = abs(y-z)
+    diff3 = abs(x-z)
+    min_diff = min(diff1, diff2, diff3)
+    return min_diff
+print(smallest_diff(32, 43, 90))
